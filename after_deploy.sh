33ac91a6799658c2243761ad2a93a172d3a237f6
Pavel <pavel.evst@gmail.com>
2011-08-24 12:05:42 +0800

    after_deploy script

--- after_deploy.sh
+++ after_deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env shell
-dotcloud run microdata.www -- "cd ~/current && rake lsd:relink_packages"
+dotcloud run microdata.www -- "cd ~/current && rake db:migrate posts:reload lsd:relink_packages && rm -r ~/current/public/javascripts/jsus/*"

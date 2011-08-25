1d4088ea441198f0c18d80eeded9027334fbdd4a
Pavel <pavel.evst@gmail.com>
2011-08-25 16:30:57 +0800

    haml multiply attributes in %tag[...]

--- config/initializers/haml.rb
+++ config/initializers/haml.rb
@@ -13,41 +13,39 @@
 class Haml::Buffer
   
   def parse_object_ref(ref)
-    p [:rel, ref]
-    prefix = ref[1]
-    ref = ref[0]
+    options = {}
     
-    # make itemprop=ref if ref if symbol
-    if ref.is_a?(Symbol)
-      return {'itemprop' => ref.to_s}
-    elsif ref.kind_of?(Mida::Vocabulary)
-      return {:itemprop => true, :itemtype => ref.itemtype.source}
-    end
-    
-    # Let's make sure the value isn't nil. If it is, return the default Hash.
-    return {} if ref.nil?
-    class_name =
-      if ref.respond_to?(:haml_object_ref)
-        ref.haml_object_ref
-      else
-        underscore(ref.class)
-      end
-    id = "#{class_name}_#{ref.id || 'new'}"
-    if prefix
-      class_name = "#{ prefix }_#{ class_name}"
-      id = "#{ prefix }_#{ id }"
-    end
-    
-    # my hack for microdata attributes
-    options = {'id' => id, 'class' => class_name}
-    if ref.respond_to?(:html_schema_type)
-      options[:itemscope] = true
-      options[:itemid] = ref.id
-      raise "No vocabulary found (#{ref.html_schema_type})" unless Mida::Vocabulary.find(ref.html_schema_type)
-      options[:itemtype] = ref.html_schema_type
+    ref.each do |obj|
+      self.class.merge_attrs(options, process_object_ref(obj))
     end
     
     options
+  end
+  
+  def process_object_ref(obj)
+    return {} if obj.nil?
+    
+    if obj.is_a?(Symbol)
+      # symbol => "itemprop" attribute
+      return {'itemprop' => obj.to_s}
+    elsif obj.kind_of?(Mida::Vocabulary)
+      # Mida::Vocabulary => itemprop and itemtype
+      return {:itemprop => true, :itemtype => obj.itemtype.source}
+    else
+      options = {}
+      options[:class] = obj.respond_to?(:haml_object_ref) ? obj.haml_object_ref : underscore(obj.class)
+      options[:id] = "#{options[:class]}_#{obj.id || 'new'}" if obj.respond_to?(:id)
+      
+      # my hack for microdata attributes
+      if obj.respond_to?(:html_schema_type)
+        options[:itemscope] = true
+        options[:itemid] = obj.id
+        raise "No vocabulary found (#{ref.html_schema_type})" unless Mida::Vocabulary.find(obj.html_schema_type)
+        options[:itemtype] = obj.html_schema_type
+      end
+      
+      return options
+    end
   end
 
 end

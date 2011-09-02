cf12bfb5dfe5f3d7d305e9771687aaa1ccaf72eb
Pavel <pavel.evst@gmail.com>
2011-09-02 15:34:07 +0800

    client optimization, delayed loading of google plus button

--- public/javascripts/Source/Application.js
+++ public/javascripts/Source/Application.js
@@ -19,46 +19,9 @@ provides: [Application]
 ...
 */
 
-/*
-What is that above?
-  It's a list of Application dependencies. It loads:
-  * LSD.Widget - The smallest entity of a framework, DOM Element's
-                 younger (and prettier) brother. A boy that will do 
-                 things for you.
-                 
-  * LSD.Document - A thing that wraps the whole document, makes
-                 <body> a widget and traverses layout to convert 
-                 elements into widgets. 
-                 
-  * LSD.Native.* - Includes a HTML4 set of widgets that handle form
-                 fields. It's a wise thing to be closer to DOM and use
-                 plain input elements.
-                 
-  * LSD.Widget...- A basic set of essensial widgets, that augment their
-                 elements to do something functional and beautiful.
-                 
-  * LSD.Action...- A set of actions that widget can execute on self, 
-                 other widgets, or plain DOM elements. There're a little
-                 more of them (e.g. Edit), but you may not need them now.
-                 
-  A prefix before slash in expression means (like Native in Native/LSD.Widget.Input)
-  is a package name (set in package.yml as "name"). So to require a file
-  from a specific package, you have to specify package name as well.
-
-Some useful things to require:
-
-  Mobile/*
-    Includes all mobile widgets and mobile adjustements if a browser
-    is detected as mobile. 
-
-  LSD/LSD.Mixin.Validity
-    Enable HTML5 form validation on all fields.
-    
-  LSD/LSD.Action.Edit
-    Adds edit action that turns a microdata-formatted element into an
-    edit in-place form with submit and cancel buttons.
-...
-*/
+$$('[data-load-script]').each(function(element) {
+  new Element('script', {src: element.get('data-load-script')}).inject(document.body);
+});
 
 LSD.Mixin.Placeholder;
 

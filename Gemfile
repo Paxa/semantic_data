e87d8db40b326de62451a95683002a707a2e37b6
Pavel <pavel.evst@gmail.com>
2011-11-09 00:27:37 +0800

    sitemap plugin

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
-source 'http://rubygems.org'
+source :rubygems
 
 gem 'rails', '3.0.10'
 gem 'mysql2', '~> 0.2.6'
-
+gem 'rake', '0.9.2.2', :require => false
 gem 'sass'
 gem 'haml', "~>3.1"
 
@@ -12,6 +12,7 @@ gem 'lsd_rails', '0.1.4', :require => 'lsd'
 gem 'mida'
 gem 'yajl-ruby', :require => 'yajl'
 gem 'coderay'
+gem 'sitemap_generator'
 
 gem 'uglifier', :require => false
 gem 'rdiscount', :require => false
---
+++ config/sitemap.rb
@@ -0,0 +1,33 @@
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "http://microdata.realitysimple.com"
+SitemapGenerator::Sitemap.yahoo_app_id = "an12x876"
+SitemapGenerator::Sitemap.create do
+  # Put links creation logic here.
+  #
+  # The root path '/' and sitemap index file are added automatically for you.
+  # Links are added to the Sitemap in the order they are specified.
+  #
+  # Usage: add(path, options={})
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #           :lastmod => Time.now, :host => default_host
+  #
+  # Examples:
+  #
+  # Add '/articles'
+  #
+  add root_path, :priority => 0.7, :changefreq => 'hourly'
+  add external_resources_path, :priority => 0.7, :changefreq => 'weekly'
+  
+  Post.find_each do |post|
+    add post_path(post), :lastmod => post.updated_at
+  end
+  
+  #
+  # Add all articles:
+  #
+  #   Article.find_each do |article|
+  #     add article_path(article), :lastmod => article.updated_at
+  #   end
+end
